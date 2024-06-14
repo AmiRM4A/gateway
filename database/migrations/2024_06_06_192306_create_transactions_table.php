10be02104008358ab98b91fe02db9bfdbd7b8099
Amir <asgry255@gmail.com>
2024-06-14 21:33:01 +0330

    :sparkles: Add Transaction Model Migrations

---
+++ database/migrations/2024_06_06_192306_create_transactions_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+            $table->string('order_id', 50)->unique()->comment('شماره سفارش پذیرنده');
+            $table->string('transaction_id',128)->unique()->comment('کلید منحصر بفرد تراکنش');
+            $table->float('amount')->unsigned()->comment('مبلغ مورد نظر ترانکش برای پرداخت');
+            $table->string('link', 128)->unique()->comment('لینک پرداخت برای انتقال خریدار به درگاه پرداخت');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('transactions');
+    }
+};
